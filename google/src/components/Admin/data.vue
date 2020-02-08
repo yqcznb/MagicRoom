9ba54909762bc89acedb33a78d3544bcaa9422ec
yqcznb <485737827@qq.com>
2020-02-08 16:32:37 +0800

    个人信息完成

--- google/src/components/Admin/data.vue
+++ google/src/components/Admin/data.vue
@@ -2,39 +2,46 @@
   <div id="data">
     <div class="top">
      <div class="left">
-    <router-link class="mui-tab-item" to="/my">
-        <svg class="icon" aria-hidden="true" >
-            <use xlink:href="#iconhuabanfuben"></use>
-        </svg>
-    </router-link>
-     </div>
-     <div>
-         个人资料
-     </div>
-    <div class="right">
-     提交
+      <router-link to="/my">
+          <svg class="icon" aria-hidden="true" >
+              <use xlink:href="#iconhuabanfuben"></use>
+          </svg>
+      </router-link>
     </div>
+       <div>个人资料</div>
+    <router-link to="/my">
+      <div class="right">提交</div>
+    </router-link>
     </div>
     <div class="touxiang">
-       <div>
+       <div >
           <van-image
+          @click="change"
           round
           width="10rem"
           height="10rem"
           src="https://img.yzcdn.cn/vant/cat.jpeg"
         />
+         <van-action-sheet
+          v-model="show"
+          :actions="actions"
+          @select="onSelect"
+          cancel-text="取消"
+          @cancel="onCancel"
+          close-on-click-action
+        />
        </div>
         <div class="info">
         <div>
-          姓名：古飞扬
+          姓名：<input type="text" name="name" id="name" v-model="name">
         </div>
         <div>
-          职位：教务处主任
+          职位：<input type="text" name="position" id="position" v-model="position">
         </div>
         <div>
-          工号：202000000
+          工号：<input type="text" name="worknum" id="worknum" v-model="worknum">
         </div> 
-    </div>
+        </div>
     </div>
     <router-view/>
   </div>
@@ -46,7 +53,12 @@ export default{
     return{
       worknum:2700001111111,
       position:'教务处主任',
-      name:"古飞扬"
+      name:"古飞扬",
+      show: false,
+      actions: [
+        { name: '拍照' },
+        { name: '从手机相册选择' },
+      ]
     }
   },
   components:{
@@ -56,7 +68,19 @@ export default{
     
    },
    methods:{
-      
+      change() {
+        this.show = true;
+        console.log(this.show)
+      },
+      onCancel() {
+        this.show = false;
+        this.$toast("取消成功");
+        console.log(this.show)
+      },
+      onSelect(item) {
+      this.show = false;
+      this.$toast(item.name);
+    }
    }
 }
 </script>
@@ -80,6 +104,7 @@ export default{
     .right{
       float: right;
       margin-right: 0.5em;
+      color:black;
     }
   }
   .touxiang{
@@ -90,7 +115,14 @@ export default{
        text-align: left;
        font-weight: bold;
        border-top: 1px solid #eee;
-       padding:0.5em 0;
+       padding:1em 2em;
+       input{
+         border: 0;
+         background: #FFFDF9;
+       }
+     }
+     div:last-child{
+        border-bottom: 1px solid #eee;
      }
     }
   }
