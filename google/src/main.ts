78e048d094ca1a10debfa12ba601c3329f20e43d
hjxw <1367464114@qq.com>
2020-02-14 21:26:35 +0800

    2020.2.14

--- google/src/main.ts
+++ google/src/main.ts
@@ -5,7 +5,7 @@ import router from './router'
 import store from './store'
 import Vant from 'vant';
 import 'vant/lib/index.css';
-import VueLazyload from 'vue-lazyload'
+import VueLazyload from 'vue-lazyload';
 import Bscroll from 'better-scroll'
 
 Vue.use(Vant);
