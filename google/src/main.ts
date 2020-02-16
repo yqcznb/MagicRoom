b4a500882c360d181c31c7030bd380bd02a2a8b1
yqcznb <485737827@qq.com>
2020-02-16 12:36:50 +0800

    人道至尊完

--- google/src/main.ts
+++ google/src/main.ts
@@ -3,8 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import VueLazyload from 'vue-lazyload'
 import Bscroll from 'better-scroll'
 
