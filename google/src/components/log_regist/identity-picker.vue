def4e047c80276579a1e66427da0c50338b53ba3
F-nothing <babalalw@gmail.com>
2020-05-09 19:55:45 +0800

    重建登录注册

---
+++ google/src/components/log_regist/identity-picker.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="identity-picker">
+        <van-picker
+        show-toolbar
+        :columns="identity_list"
+        :default-index = '1'
+        @confirm = "confirmIdentity"
+        @cancel = "cancelIdentity"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'identity_picker',
+    data() {
+        return {
+            identity_list: [
+                '老师',
+                '学生',
+                '管理员'
+            ]
+        }
+    },
+    methods: {
+        confirmIdentity(value, index) {
+            this.$emit('confirm-identity', value);
+        },
+        cancelIdentity() {
+            this.$emit('cancel-identity');
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.identity-picker {
+
+}
+</style>
